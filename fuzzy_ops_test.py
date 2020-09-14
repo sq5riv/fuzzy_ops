4806875e8f22868ddaa8d26fcda90ccb01446458
sq5riv <40839874+sq5riv@users.noreply.github.com>
2020-09-14 14:58:10 +0200

    First test_check
    First test

---
+++ fuzzy_ops_test.py
@@ -0,0 +1,11 @@
+import unittest
+from fuzzy_ops import fuzzy_set
+
+class TestData(unittest.TestCase):
+    def in_data(self):
+        a = list(range(10))
+        b = [0.1]
+        c = list(range(10))
+        self.assertRaises(ValueError, a,b,c)
+        self.assertRaises(TypeError, 'a','a','a')
+        
