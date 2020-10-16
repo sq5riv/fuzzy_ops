4249cc53f011bc5d262893eeac3ec996cb2bb3bd
sq5riv <40839874+sq5riv@users.noreply.github.com>
2020-10-16 14:56:01 +0200

    Add sum and Tnorms

--- fuzzy_ops2.py
+++ fuzzy_ops2.py
@@ -18,29 +18,28 @@ class fuzzy_set(object):
         self.made_by_func = None
         #check for data correct
         self._check1()
-        self._alphator()
+        if self.made_by_func == True:
+            self._alphator()
 
     def _alphator(self):
         '''makes alphacuts from function'''
-
-        for alpha in self._alphas:
+        
+        for alpha in self._alpha:
             tmp_list = []
             isup = False
-            for i in enumerate(len(self(domain))):
+            for i in range(len(self._domain)):
                 if self._function[i]>=alpha and isup == False:
                     tmp_list.append(self._domain[i])
                     isup = True
                 elif self._function[i]<alpha and isup == True:
                     tmp_list.append(self._domain[i-1])
                     isup = False
-            if len(tmp)%2 == 1:
+            if len(tmp_list)%2 == 1:
                 tmp_list.append(self._domain[-1])
             self._alpha_dict[alpha] = tmp_list
 
-        self.is_convex()
-            
-            
-                           
+        self._is_convex()
+
     def _c1(self, data, name):
         '''checks is None or list or tuple'''
 
@@ -79,14 +78,14 @@ class fuzzy_set(object):
         '''check for alphacuts dictionary is correst'''
 
         if not  isinstance(self._alpha_dict, dict):
-            raise: TypeError('alpha_dict have to by dict')
+            raise TypeError('alpha_dict have to by dict')
         if self.made_by_func == False:
             for k,v in self._alpha_dict.items():
-                if k<1 and k>0: raise ValueError('Alpha levels have to be between 0 and 1')
+                if k>1 or k<0: raise ValueError('Alpha levels have to be between 0 and 1')
 
-                if not isinstance(v, tuple): raise TypeError('Alphacuts have to be tuple')
+                if not isinstance(v, (tuple, list)): raise TypeError('Alphacuts have to be tuple or list')
 
-    def is_convex(self):
+    def _is_convex(self):
         '''check for fuzzy set is convex'''
 
         for k,v in self._alpha_dict.items():
@@ -109,10 +108,98 @@ class fuzzy_set(object):
         self._c5()
         self._c6()
         if self.made_by_func == False:
-            self.is_convex()
+            self._is_convex()
 
+    def get_Acuts(self):
+        '''returns alpha cuts'''
+
+        return self._alpha_dict.copy()
+    
+    def comp_sum(self,a,b):
+        '''returns sum of compatrments'''
+#Cy to tak ma być?
+        c = a[0]+b[0]
+        d =a[1]+b[1]
+
+        return [c,d]
+        
+    def fuzzy_sum(self, fuzzy=None, tnorm=None, alphas = None):
+        '''make sum of two fuzzy sets with some tnorm'''
+
+        if not isinstance(fuzzy, fuzzy_set): raise TypeError('Fuzzy set object is needed''')
+        if alphas == None:
+            tmp_alphas=self._alpha
+        else:
+            tmp_alphas=alphas
+            
+        loc_alphacuts = self._alpha_dict.copy()
+        rem_alphacuts = fuzzy.get_Acuts()
+        tmp_alphas.sort(reverse=True)
+        new_alpha_dict = {}
+        last_vals = []
+        tmp_vals = []
+        last_alpha = 2
+        for alpha in tmp_alphas:
+            for k1, v1 in loc_alphacuts.items():
+                for k2,v2 in rem_alphacuts.items():
+                    if tnorm(k1,k2) >= alpha :
+                        tmp_vals.extend(self.comp_sum(v1,v2))
+            tmp_vals.extend(last_vals)
+            last_vals = [min(tmp_vals),max(tmp_vals)]
+            new_alpha_dict[alpha] =last_vals
+        print(new_alpha_dict)
+        return fuzzy_set(alpha_dict=new_alpha_dict)
+                        
+
+        
+def T_min(a,b):
+    '''reurns min of  arguments'''
+
+    return min(a,b)
+
+def T_product(a,b):
+    '''returns a*b'''
+
+    return a*b
+
+def T_Luk(a,b):
+    '''returns lukasiewicz Tnorm'''
+
+    return  max(0, a+b-1)
+
+def T_drastic(a,b):
+    '''returns drastic T_norm'''
+
+    if a==1: return b
+    elif b==1:return a
+    else: return 0
+
+def T_nil(a,b):
+    '''returns nilpolent T-norm'''
+
+    if a+b>1: return min(a,b)
+    else: return 0
+
+def T_ham(a,b):
+    '''returns hamacher T_norm'''
+
+    if a == b and a == 0: return 0
+    else: return (a*b)/(a+b-a*b)
+
+def T_sklar(a,b,p):
+    '''returns Sklar T-norm'''
+
+    if p == float('-inf'): return min(a,b)
+    elif p > float('-inf') and  p<0: return (x**p+y**p-1)**(1/p)
+    elif p == 0: return a*b
+    elif p>0 and p<float('inf'):return max(0, (x**p+y**p-1)**(1/p))
+    elif p == float('inf'): return T_drastic(a,b)
+    
 if __name__ == '__main__':
     a = [1,2,3,4,5,6,7,8,9,10]
-    b =[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+    b =[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.3]
     c= [0.1,0.3,0.5,0.9]
     z = fuzzy_set(a,b,c)
+    print(z.get_Acuts())
+    q = z.fuzzy_sum(z,T_min)
+    print(q.get_Acuts())
